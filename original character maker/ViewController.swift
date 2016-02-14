2bbcec5dfbc5890a67cb7d7ecf79734052f104a6
金森彦磨呂 <majorleorena@gmail.com>
2016-02-14 16:47:26 +0900

    changecollectionview

--- original character maker/ViewController.swift
+++ original character maker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIActionSheetDelegate {
+class ViewController: UIViewController, UIActionSheetDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet var noseImageView: UIImageView!
     @IBOutlet var eyeImageView: UIImageView!
@@ -18,8 +18,12 @@ class ViewController: UIViewController, UIActionSheetDelegate {
     @IBOutlet var part2Bt: UIButton!
     @IBOutlet var part3Bt: UIButton!
     @IBOutlet var part4Bt: UIButton!
+    @IBOutlet var myCollectionView : UICollectionView!
     
     var number: Int = 0
+    
+    var imageArray:[String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +32,9 @@ class ViewController: UIViewController, UIActionSheetDelegate {
         longPressGesture.minimumPressDuration = 1.0
         longPressGesture.allowableMovement = 150
         self.view.addGestureRecognizer(longPressGesture)
+        myCollectionView.delegate = self
+        myCollectionView.dataSource = self
+        imageArray = ["eye1.png", "eye2.png","eye3.png","eye4.png"]
     }
     func longPressGesture(sender: UILongPressGestureRecognizer){
         if sender.state == UIGestureRecognizerState.Began {
@@ -41,6 +48,50 @@ class ViewController: UIViewController, UIActionSheetDelegate {
         }
     }
     
+    //MARK: collectionViewの設定
+    /*
+    Cellが選択された際に呼び出される
+    */
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        switch number {
+        case 1:
+            let imageName = "eye" + String(indexPath.row + 1) + ".png"
+            eyeImageView.image = UIImage(named: imageName)
+        case 2:
+            let imageName = "nose" + String(indexPath.row + 1) + ".png"
+            noseImageView.image = UIImage(named: imageName)
+        case 3:
+            let imageName = "hair" + String(indexPath.row + 1) + ".png"
+            hairImageView.image = UIImage(named: imageName)
+        default:
+            break
+        }
+
+        
+    }
+    
+    /*
+    Cellの総数を返す
+    */
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    /*
+    Cellに値を設定する
+    */
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell : UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell",
+            forIndexPath: indexPath)
+        let imageView = cell.viewWithTag(1) as! UIImageView
+        imageView.image = UIImage(named: imageArray[indexPath.row])
+        
+        
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -48,28 +99,37 @@ class ViewController: UIViewController, UIActionSheetDelegate {
     
     @IBAction func tapEyeBt(){
         //ボタンの画像を切り替える
-        part1Bt.setBackgroundImage(UIImage(named: "eye1.png"), forState: UIControlState.Normal)
-        part2Bt.setBackgroundImage(UIImage(named: "eye2.png"), forState: UIControlState.Normal)
-        part3Bt.setBackgroundImage(UIImage(named: "eye3.png"), forState: UIControlState.Normal)
-        part4Bt.setBackgroundImage(UIImage(named: "eye4.png"), forState: UIControlState.Normal)
+//        part1Bt.setBackgroundImage(UIImage(named: "eye1.png"), forState: UIControlState.Normal)
+//        part2Bt.setBackgroundImage(UIImage(named: "eye2.png"), forState: UIControlState.Normal)
+//        part3Bt.setBackgroundImage(UIImage(named: "eye3.png"), forState: UIControlState.Normal)
+//        part4Bt.setBackgroundImage(UIImage(named: "eye4.png"), forState: UIControlState.Normal)
+        imageArray = ["eye1.png", "eye2.png","eye3.png","eye4.png"]
+        myCollectionView.reloadData()
+
         number = 1
         
     }
     @IBAction func tapNoseBt(){
         //ボタンの画像を切り替える
-        part1Bt.setBackgroundImage(UIImage(named: "nose1.png"), forState: UIControlState.Normal)
-        part2Bt.setBackgroundImage(UIImage(named: "nose2.png"), forState: UIControlState.Normal)
-        part3Bt.setBackgroundImage(UIImage(named: "nose3.png"), forState: UIControlState.Normal)
-        part4Bt.setBackgroundImage(UIImage(named: "nose4.png"), forState: UIControlState.Normal)
+//        part1Bt.setBackgroundImage(UIImage(named: "nose1.png"), forState: UIControlState.Normal)
+//        part2Bt.setBackgroundImage(UIImage(named: "nose2.png"), forState: UIControlState.Normal)
+//        part3Bt.setBackgroundImage(UIImage(named: "nose3.png"), forState: UIControlState.Normal)
+//        part4Bt.setBackgroundImage(UIImage(named: "nose4.png"), forState: UIControlState.Normal)
+        imageArray = ["nose1.png", "nose2.png","nose3.png","nose4.png"]
+        myCollectionView.reloadData()
+
         number = 2
         
     }
     @IBAction func tapHairBt(){
         //ボタンの画像を切り替える
-        part1Bt.setBackgroundImage(UIImage(named: "hair1.png"), forState: UIControlState.Normal)
-        part2Bt.setBackgroundImage(UIImage(named: "hair2.png"), forState: UIControlState.Normal)
-        part3Bt.setBackgroundImage(UIImage(named: "hair3.png"), forState: UIControlState.Normal)
-        part4Bt.setBackgroundImage(UIImage(named: "hair4.png"), forState: UIControlState.Normal)
+//        part1Bt.setBackgroundImage(UIImage(named: "hair1.png"), forState: UIControlState.Normal)
+//        part2Bt.setBackgroundImage(UIImage(named: "hair2.png"), forState: UIControlState.Normal)
+//        part3Bt.setBackgroundImage(UIImage(named: "hair3.png"), forState: UIControlState.Normal)
+//        part4Bt.setBackgroundImage(UIImage(named: "hair4.png"), forState: UIControlState.Normal)
+        imageArray = ["hair1.png", "hair2.png","hair3.png","hair4.png"]
+        myCollectionView.reloadData()
+
         number = 3
         
     }
@@ -111,7 +171,7 @@ class ViewController: UIViewController, UIActionSheetDelegate {
     }
     
     //保存ボタンを押したら
-    @IBAction func tapImageBt(){
+        @IBAction func tapImageBt(){
         //ビットマップ形式のグラフィックスコンテキストの生成
         UIGraphicsBeginImageContextWithOptions(CGSizeMake(288, 288), true,0)
         let imageArray: [UIImageView?] = [facelineImageView, noseImageView,hairImageView,eyeImageView]
